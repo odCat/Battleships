a5f07ae201719d54174e41362d8615b8e3a338c7
Mihai Gătejescu <mgatejescu@pm.me>
2025-01-07 14:13:15 +0200

    Add DotCom class

---
+++ DotCom.java
@@ -0,0 +1,24 @@
+class DotCom
+{
+    short [] locations = {2, 3, 4};
+    short numOfHits = 0;
+
+    public String checkHit(String guess)
+    {
+        int userGuess = Integer.parseInt(guess);
+        String result = "miss";
+
+        for (int cell : locations)
+            if (userGuess == cell) {
+                result = "hit";
+                ++numOfHits;
+                break;
+            }
+
+        if (numOfHits == localtions.length)
+            result = "kill"
+
+        return result;
+    }
+}
+
