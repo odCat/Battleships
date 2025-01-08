e94cb322b732ccc2c78217e6eab8691dbeb0ab78
Mihai Gătejescu <mgatejescu@pm.me>
2025-01-08 16:27:09 +0200

    Play the game

--- DotComGame.java
+++ DotComGame.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
+
 class DotComGame
 {
     public static void main(String [] args)
     {
+        short numOfGuesses = 0;
         DotCom dotCom = new DotCom();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Take a guess: ");
+            String guess = scan.next();
+            String result = dotCom.checkHit(guess);
+            ++numOfGuesses;
+            if (result.equals("kill")) {
+                System.out.println("Kill");
+                break;
+            } else {
+                System.out.println(result);
+            }
+        }
+
+        System.out.println("You took " + numOfGuesses + " guesses");
     }
 }
