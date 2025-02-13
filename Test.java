790893a19fba27599fc57a8a29075b08c58e8436
Mihai Gătejescu <mgatejescu@pm.me>
2025-02-13 21:12:12 +0200

    Update the test variable name to battleship

--- Test.java
+++ Test.java
@@ -16,24 +16,24 @@ class Test {
         locations.add(new Cell(2, 1));
         locations.add(new Cell(3, 1));
         locations.add(new Cell(4, 1));
-        Battleship dot = new Battleship(locations, 1);
+        Battleship battleship = new Battleship(locations, 1);
 
-        if (dot.checkHit("11").equals("miss"))
+        if (battleship.checkHit("11").equals("miss"))
             result = "pass";
         else
             result = "fail";
 
         System.out.println(result);
 
-        if (dot.checkHit("21").equals("hit"))
+        if (battleship.checkHit("21").equals("hit"))
             result = "pass";
         else
             result = "fail";
 
         System.out.println(result);
 
-        dot.checkHit("31");
-        if (dot.checkHit("41").equals("kill"))
+        battleship.checkHit("31");
+        if (battleship.checkHit("41").equals("kill"))
             result = "pass";
         else
             result = "fail";
@@ -49,10 +49,10 @@ class Test {
         locations.add(new Cell(2, 1));
         locations.add(new Cell(3, 1));
         locations.add(new Cell(4, 1));
-        Battleship dot = new Battleship(locations, 2);
+        Battleship battleship = new Battleship(locations, 2);
 
-        dot.checkHit("21");
-        if (dot.checkHit("21").equals("miss"))
+        battleship.checkHit("21");
+        if (battleship.checkHit("21").equals("miss"))
             result = "pass";
         else
             result = "fail";
