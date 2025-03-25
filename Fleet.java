7bf07c369effa6e4ff24027a3f474ad46384945e
Mihai Gătejescu <mgatejescu@pm.me>
2025-03-25 13:52:35 +0200

    Accept a fleet size

--- Fleet.java
+++ Fleet.java
@@ -5,11 +5,17 @@ class Fleet {
     static final int DEFAULT_FLEET_SIZE = 3;
     final ArrayList<Battleship> fleet = initFleet();
 
+
     public static ArrayList<Battleship> initFleet()
     {
-        ArrayList<Battleship> result = new ArrayList<>(DEFAULT_FLEET_SIZE);
+        return initFleet(DEFAULT_FLEET_SIZE);
+    }
 
-        for (int i = 0; i < DEFAULT_FLEET_SIZE; ++i) {
+    public static ArrayList<Battleship> initFleet(int fleetSize)
+    {
+        ArrayList<Battleship> result = new ArrayList<>(fleetSize);
+
+        for (int i = 0; i < fleetSize; ++i) {
             if (i == 0) {
                 Battleship newShip = new Battleship();
                 result.add(newShip);
